2f1dea8d7e885ebb40d59ee103c5a2a3a7bfca78
Mikolaj <mikolajmuchowicz@gmail.com>
2023-04-30 16:14:37 +0200

    Second Test

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -19,7 +20,25 @@ public class UltimateQATest extends PageSetup{
 
     }
 
+    @Test
+    public void  testTwo() throws InterruptedException {
 
+        WebElement nameInput = driver.findElementById("et_pb_contact_name_0");
+        nameInput.sendKeys("Tester");
+
+        WebElement emailInput = driver.findElementById("et_pb_contact_email_0");
+        emailInput.sendKeys("tester@tester.pl");
+
+        Thread.sleep(2000);
+
+        WebElement emailMeButton = driver.findElementByName("et_builder_submit_button");
+        emailMeButton.click();
+
+        Thread.sleep(2000);
+
+        WebElement thanksText = driver.findElement(By.xpath("//div[@class='et-pb-contact-message']/p"));
+        Assertions.assertEquals("Thanks for contacting us", thanksText.getText());
+    }
 
 
 }
